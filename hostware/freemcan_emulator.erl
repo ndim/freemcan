7882b8b461fc79f701ceed637bae2afdbf843ee2
Hans Ulrich Niedermann <hun@n-dimensional.de>
2010-02-18 18:21:08 +0100

    Add some debugging information

--- hostware/freemcan_emulator.erl
+++ hostware/freemcan_emulator.erl
@@ -9,15 +9,18 @@
 loop(State = #state{port=Port}) ->
     receive
 	{Port, {data, CmdBin}} ->
+	    io:format("Port info:        ~p~n", [erlang:port_info(Port)]),
 	    io:format("Received command: ~p~n", [CmdBin]),
 	    Reply = <<"Moo">>,
 	    io:format("Sending reply:    ~p~n", [Reply]),
 	    Port ! {self(), {command, Reply}},
 	    loop(State);
 	{'EXIT', Port, Reason} ->
+	    io:format("Port info:        ~p~n", [erlang:port_info(Port)]),
 	    io:format("EXIT on Port:     ~p~n", [Reason]),
 	    {error, Reason};
 	Unhandled ->
+	    io:format("Port info:        ~p~n", [erlang:port_info(Port)]),
 	    io:format("Unhandled:        ~p~n", [Unhandled]),
 	    loop(State)
     end.
@@ -26,11 +29,24 @@ main(FIFO) ->
     io:format("FIFO=~s~n", [FIFO]),
     {ok, Cwd} = file:get_cwd(),
     ExecName = "erl_unix_port",
-    Executable = filename:join([Cwd, ExecName]),
+    {Executable, Arg0, Args} =
+	case true of
+	    false ->
+		{filename:join([Cwd, ExecName]),
+		 ExecName,
+		 [FIFO]};
+	    true ->
+		{"/usr/bin/strace",
+		 "strace",
+		 ["-s1024",
+		  "-oerl_unix_port.strace",
+		  filename:join([Cwd, ExecName]),
+		  FIFO]}
+	end,
     Port = open_port({spawn_executable, Executable},
 		     [nouse_stdio, binary, stream,
-		      {arg0, ExecName},
-		      {args, [FIFO]}
+		      {arg0, Arg0},
+		      {args, Args}
 		     ]),
     io:format("Port: ~p~n", [Port]),
     loop(#state{port=Port}).

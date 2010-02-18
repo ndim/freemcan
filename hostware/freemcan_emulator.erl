cdddc9a05fb7256ee1135bd3a499d2b7188f1f86
Hans Ulrich Niedermann <hun@n-dimensional.de>
2010-02-18 20:44:54 +0100

    Try getting emulator to update w/o restart

--- hostware/freemcan_emulator.erl
+++ hostware/freemcan_emulator.erl
@@ -28,54 +28,57 @@
 
 -record(state, {port, state=boot, timeout=100}).
 
-next(boot, {timeout, _}) ->
-    {start, <<"Booted.\n">>, ?DEFAULT_TIMEOUT};
+fsm(boot, {timeout, _}) ->
+    {start, <<"Booted.\n">>, none};
 
-next(start, <<"r">>) ->
-    {reset, none, 10};
-next(start, <<"m">>) ->
+fsm(start, <<"r">>) ->
+    {reset, none, 100};
+fsm(start, <<"m">>) ->
     {{measuring, 0}, <<"Started measurement\n">>, 10000};
-next(start, {timeout, _}) ->
-    {start, none, ?DEFAULT_TIMEOUT};
 
-next({measuring, N}, {timeout, _}) when is_integer(N), N =< 3 ->
+fsm({measuring, N}, {timeout, _}) when is_integer(N), N =< 3 ->
     {{measuring, N+1}, <<"Still measuring\n">>, 10000};
-next({measuring, _}, {timeout, _}) ->
-    {reset, <<"Measurement finished\n">>, 10000};
+fsm({measuring, _}, {timeout, _}) ->
+    {reset, <<"Measurement finished\n">>, 0};
 
-next(reset, {timeout, _}) ->
+fsm(reset, {timeout, _}) ->
     {boot, <<"Resetting\n">>, 100}.
 
+
 loop(LoopState = #state{port=Port, state=CurState, timeout=TimeOut}) ->
+    RealTimeOut = case TimeOut of
+		      none -> 100000;
+		      N when is_integer(N) -> N
+		  end,
     receive
 	{Port, {data, Cmd}} ->
 	    io:format("Port info:        ~p~n", [erlang:port_info(Port)]),
 	    io:format("Received command: ~p~n", [Cmd]),
-	    {NextState, Reply, NextTimeOut} = next(CurState, Cmd),
+	    {NextState, Reply, NextTimeOut} = fsm(CurState, Cmd),
 	    io:format("Sending reply:    ~p~n", [Reply]),
 	    Port ! {self(), {command, Reply}},
 	    loop(LoopState#state{state=NextState, timeout=NextTimeOut});
-	{'EXIT', Port, Reason} ->
-	    io:format("Port info:        ~p~n", [erlang:port_info(Port)]),
-	    io:format("EXIT on Port:     ~p~n", [Reason]),
-	    {error, Reason};
 	Unhandled ->
 	    io:format("Port info:        ~p~n", [erlang:port_info(Port)]),
 	    io:format("Unhandled:        ~p~n", [Unhandled]),
 	    {error, {unhandled, Unhandled}}
-    after TimeOut ->
-	    {NextState, Reply, NextTimeOut} = next(CurState, {timeout, TimeOut}),
-	    io:format("Timeout:          ~p~n", [TimeOut]),
-	    io:format("Sending reply:    ~p~n", [Reply]),
-	    case Reply of
-		none -> ok;
-		Reply ->
-		    Port ! {self(), {command, Reply}}
-	    end,
-	    loop(LoopState#state{state=NextState, timeout=NextTimeOut})
+    after RealTimeOut ->
+	    case TimeOut of
+		none -> loop(LoopState);
+		TimeOut ->
+		    {NextState, Reply, NextTimeOut} = fsm(CurState, {timeout, TimeOut}),
+		    io:format("Timeout:          ~p~n", [TimeOut]),
+		    io:format("Sending reply:    ~p~n", [Reply]),
+		    case Reply of
+			none -> ok;
+			Reply ->
+			    Port ! {self(), {command, Reply}}
+		    end,
+		    loop(LoopState#state{state=NextState, timeout=NextTimeOut})
+	    end
     end.
 
-main(FIFO) ->
+init(FIFO) ->
     io:format("FIFO=~s~n", [FIFO]),
     {ok, Cwd} = file:get_cwd(),
     ExecName = "erl_unix_port",
@@ -99,12 +102,16 @@ main(FIFO) ->
 		      {args, Args}
 		     ]),
     io:format("Port: ~p~n", [Port]),
-    loop(#state{port=Port}).
+    #state{port=Port}.
+
+intermain(FIFO) ->
+    InitState = init(FIFO),
+    spawn(?MODULE, loop, [InitState]).
 
 start() ->
     start([]).
 
 start([FIFO]) when is_atom(FIFO) ->
-    main(atom_to_list(FIFO));
+    intermain(atom_to_list(FIFO));
 start([FIFO]) when is_list(FIFO) ->
-    main(FIFO).
+    intermain(FIFO).

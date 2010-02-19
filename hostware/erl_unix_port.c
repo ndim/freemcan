ceb0da722ba55a7c557ddcdc007335e9a9245e3b
Hans Ulrich Niedermann <hun@n-dimensional.de>
2010-02-19 13:19:49 +0100

    Add copyright+license information
    Add copyright+license headers to all source files:
    
       *.c *.erl *.h *.S
    
    Also add the full license text for the two licenses used:
    
       lgpl-2.1.txt: LGPL v2.1
       EPLICENSE:    Erlang Public License 1.1

--- hostware/erl_unix_port.c
+++ hostware/erl_unix_port.c
@@ -1,3 +1,22 @@
+/** \file erl_unix_port.c
+ * \brief Specialized Erlang port to interface to AF_UNIX sockets
+ *
+ * \author Copyright (C) 2010 Hans Ulrich Niedermann <hun@n-dimensional.de>
+ *
+ * The contents of this file are subject to the Erlang Public License,
+ * Version 1.1, (the "License"); you may not use this file except in
+ * compliance with the License. You should have received a copy of the
+ * Erlang Public License along with this software. If not, it can be
+ * retrieved via the world wide web at http://www.erlang.org/.
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Initial Developer of the Original Code is Hans Ulrich Niedermann.
+ */
+
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>

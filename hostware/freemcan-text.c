ec600fd23f29d3868cd9787d2844161ae34b3c47
Hans Ulrich Niedermann <hun@n-dimensional.de>
2010-02-21 01:06:45 +0100

    Note bug leaving TTY messed up

--- hostware/freemcan-text.c
+++ hostware/freemcan-text.c
@@ -2,6 +2,8 @@
  * \brief Freemcan interactive text user interface (non-ncurses)
  * \author Copyright (C) 2010 Hans Ulrich Niedermann <hun@n-dimensional.de>
  *
+ * \bug We leave the TTY in a broken state after regular quit.
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public License
  *  as published by the Free Software Foundation; either version 2.1

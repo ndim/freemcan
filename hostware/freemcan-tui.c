c2d777d3f4717f39d1317d7ae3d2755a4f9db947
Hans Ulrich Niedermann <hun@n-dimensional.de>
2010-03-28 23:23:13 +0200

    hostware-tui: Only send char if not a local cmd
    Only send a character typed by the user on to the device if
    that character has no special meaning as a command to hostware-tui.

--- hostware/freemcan-tui.c
+++ hostware/freemcan-tui.c
@@ -282,11 +282,6 @@ void tui_do_io(void)
         fmlog("M                       send command \"start (m)easurement\" (long runtime)");
         fmlog("r                       send command \"(r)eset\"");
 	break;
-      case FRAME_CMD_ABORT:
-      case FRAME_CMD_INTERMEDIATE:
-      case FRAME_CMD_RESET:
-	tui_device_send_command(buf[i], 0);
-	break;
       case FRAME_CMD_MEASURE: /* 'm' */
 	/* "SHORT" measurement */
 	tui_device_send_command(FRAME_CMD_MEASURE, 10);
@@ -294,6 +289,11 @@ void tui_do_io(void)
       case 'M': /* 'm' */
 	/* "LONG" measurement */
 	tui_device_send_command(FRAME_CMD_MEASURE, 30);
+	break;
+      case FRAME_CMD_ABORT:
+      case FRAME_CMD_INTERMEDIATE:
+      case FRAME_CMD_RESET:
+	tui_device_send_command(buf[i], 0);
 	break;
       /* No "default:" case as we ignore all other characters. */
       }

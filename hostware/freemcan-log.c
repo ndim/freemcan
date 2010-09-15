8719dd528a36ed1e420be134b3bd3826e7c07c77
Hans Ulrich Niedermann <hun@n-dimensional.de>
2010-09-15 21:50:30 +0200

    fix signedness in fmlog_hist

--- hostware/freemcan-log.c
+++ hostware/freemcan-log.c
@@ -252,7 +252,7 @@ void fmlog_hist(const uint32_t *elements, const size_t count)
   }
 
   /* Lookup table for places per number, and numbers per line */
-  int places, perline;
+  unsigned int places, perline;
   if      (max >= 1000000000) { places = 10; perline =  4; }
   else if (max >=  100000000) { places =  9; perline =  4; }
   else if (max >=   10000000) { places =  8; perline =  8; }

6238436e06f1f8ee4b471d175d88031817f48419
Hans Ulrich Niedermann <hun@n-dimensional.de>
2010-09-27 14:39:07 +0200

    firmware: document synced reading of timer_count
    Document how we do the synced reading of the timer_count
    variable, and move that code to a new get_duration() function.
    
    Thanks to function inlining, this results in no change in
    the AVR machine code actually emitted.

--- firmware/main.c
+++ firmware/main.c
@@ -172,6 +172,8 @@ volatile uint16_t timer_count;
  * Used for pseudo synchronized reading of the timer_count multi-byte
  * variable in the main program, while timer_count may be written to
  * by the timer ISR.
+ *
+ * \see get_duration, ISR(TIMER1_COMPA_vect)
  */
 volatile uint16_t last_timer_count = 1;
 
@@ -265,6 +267,11 @@ ISR(ADC_vect) {
  *
  * When timer has elapsed, the global #timer_flag (8bit, therefore
  * atomic read/writes) is set.
+ *
+ * Note that we are counting down the timer_count, so it will start
+ * with its maximum value and count down to zero.
+ *
+ * \see last_timer_count, get_duration
  */
 ISR(TIMER1_COMPA_vect)
 {
@@ -279,6 +286,73 @@ ISR(TIMER1_COMPA_vect)
       timer_flag = 1;
     }
   }
+}
+
+
+
+/** Get elapsed time in the currently running or finished measurement
+ *
+ * We do synchronized reading of the multi-byte variable timer_count
+ * here (which is being written to by the ISR(TIMER1_COMPA_vect) while
+ * send_histogram() is being executed for 'I' histograms). For all other
+ * types of histograms, this busy sync loop will still work, but is not
+ * required as all interrupts will be disabled.
+ *
+ * Reading both #timer_count and #last_timer_count consists basically of
+ * the following four steps:
+ *
+ *   a) read lo8(#timer_count)
+ *
+ *   b) read hi8(#timer_count)
+ *
+ *   c) read lo8(#last_timer_count)
+ *
+ *   d) read hi8(#last_timer_count)
+ *
+ * Now we have a finite set of sequences in which those instructions
+ * and ISR(TIMER1_COMPA_vect) can be executed in relation to each
+ * other (keep in mind that the #timer_count is counted backwards):
+ *
+ *  1. ISR before a): No problem whatsoever.  #last_timer_count will
+ *     be 1 more than #timer_count.  The while loop will finish.
+ *
+ *  2. ISR between a) and b): #timer_count will differ from
+ *     #last_timer_count by two, or a lot more in the case of a 8bit
+ *     overflow happening.  The while loop will continue.
+ *
+ *  3. ISR between b) and c): #timer_count will be the same as
+ *     #last_timer_count.  The while loop will continue.
+ *
+ *  4. ISR between c) and d): #timer_count will be the same as
+ *     #last_timer_count (just like case 3), or a lot more in the case
+ *     of a 8bit overflow happening.  The while loop will continue.
+ *
+ *  5. ISR after d): No problem whatsoever.  #last_timer_count will be
+ *     1 more than #timer_count.  The while loop will finish.
+ *
+ * As the ISR runs only every second, and we can reasonably presume
+ * that the while loop can repeats a number of times during that
+ * second, this will terminate quite quickly with a useful result.
+ *
+ * The result may be off by one for the 'I' histograms but not by
+ * more, and for 'I' results we can tolerate that kind of inaccuracy.
+ *
+ * Durations for finished measurements will always be accurate, as
+ * that will trigger case 1.
+ *
+ * \see last_timer_count, ISR(TIMER1_COMPA_vect)
+ */
+inline static
+uint16_t get_duration(void)
+{
+  uint16_t a, b;
+  do {
+    a = timer_count;
+    b = last_timer_count;
+  } while ((b-a) != 1);
+  /* Now 'a' contains a valid value. Use it. */
+  const uint16_t duration = orig_timer_count - a;
+  return duration;
 }
 
 
@@ -494,21 +568,15 @@ void invent_histogram(const uint16_t duration)
  * the timer.  We are currently keeping interrupts enabled if we
  * continue measuring, which avoids this issue.
  *
+ * Note that for 'I' histograms it is possible that we send fluked
+ * values due to overflows.
  */
 static
 void send_histogram(const packet_histogram_type_t type);
 static
 void send_histogram(const packet_histogram_type_t type)
 {
-  /* pseudo synchronised reading of multi-byte variable being written
-   * to by ISR */
-  uint16_t a, b;
-  do {
-    a = timer_count;
-    b = last_timer_count;
-  } while ((b-a) != 1);
-  /* Now 'a' contains a valid value */
-  const uint16_t duration = orig_timer_count - a;
+  const uint16_t duration = get_duration();
 
 #ifdef INVENTED_HISTOGRAM
   invent_histogram(duration);

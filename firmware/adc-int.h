cac27121c64d2267e2fc867d6a39e6d7103551ac
Hans Ulrich Niedermann <hun@n-dimensional.de>
2010-10-02 14:34:39 +0200

    firmware: Move internal ADC code to adc-int.[ch]
    This includes all code specific to talking to the hardware setup
    which used the AVR's internal ADC for doing MCA.

---
+++ firmware/adc-int.h
@@ -0,0 +1,56 @@
+/** \file firmware/adc-int.h
+ * \brief Internal ADC
+ *
+ * \author Copyright (C) 2010 samplemaker
+ * \author Copyright (C) 2010 Hans Ulrich Niedermann <hun@n-dimensional.de>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1
+ *  of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free
+ *  Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *  Boston, MA 02110-1301 USA
+ *
+ * \addtogroup adc_int
+ * @{
+ */
+
+#ifndef ADC_INT_H
+#define ADC_INT_H
+
+
+#include "global.h"
+
+
+/** Number of elements in the histogram table */
+#define MAX_COUNTER (1<<ADC_RESOLUTION)
+
+
+/** \var table
+ * \brief histogram table
+ *
+ * ATmega644P has 4Kbyte RAM.  When using 10bit ADC resolution,
+ * MAX_COUNTER==1024 and 24bit values will still fit (3K table).
+ **/
+volatile histogram_element_t table[MAX_COUNTER];
+
+
+/** @} */
+
+#endif /* ADC_INT_H */
+
+
+/*
+ * Local Variables:
+ * c-basic-offset: 2
+ * indent-tabs-mode: nil
+ * End:
+ */

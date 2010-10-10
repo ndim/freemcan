ndim/freemcan

commit 670af9531021a308d576268e420f7565b163ca06

firmware: Detect unsupported timer count
If we are given an unsupported timer_count value
of 0 or 1, we send an error message, abort and reboot.

firmware/measurement-timer.h
@@ -112,6 +112,12 @@ void timer_init(const uint8_t timer0, const uint8_t timer1)
         [timer1] "r" (timer1)
       );
 
+  /** Safeguard: We cannot handle 0 or 1 count measurements. */
+  if (orig_timer_count <= 1) {
+    send_text("Unsupported timer value <= 1");
+    wdt_soft_reset();
+  }
+
   /* Prepare timer 0 control register A and B for
      clear timer on compare match (CTC)                           */
   TCCR1A = 0;

commit 3adf6e06800a7d173e0ea8c8bba9d93730eafb7c

firmware: Add #includes for required prototypes
This fixes an issue introduced by

    670af953 firmware: Detect unsupported timer count

firmware/measurement-timer.h
@@ -33,6 +33,8 @@
 #include <avr/interrupt.h>
 
 #include "global.h"
+#include "packet-comm.h"
+#include "wdt-softreset.h"
 
 
 /** timer counter
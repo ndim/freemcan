26573dec8a864192f014b7d41fab0f23cfabf1b5
Hans Ulrich Niedermann <hun@n-dimensional.de>
2010-10-17 11:18:09 +0200

    firmware: Shut up geiger-ts startup messages
    Only print one value (if compiled with printf support):
    The size of the table in elements.

--- firmware/geiger-time-series.c
+++ firmware/geiger-time-series.c
@@ -142,16 +142,21 @@ void ts_print_status(void)
   __attribute__ ((section(".init8")));
 void ts_print_status(void)
 {
+  const size_t UV(sizeof_table) = ((char*)table_end) - ((char*)table_cur);
+#ifdef VERBOSE_STARTUP_MESSAGES
   uprintf("<ts_print_status>");
   uprintf("%-25s %p", "data_table", data_table);
   uprintf("%-25s %p", "table_cur",  table_cur);
   uprintf("%-25s %p", "table_end",  table_end);
-  const size_t UV(sizeof_table) = ((char*)table_end) - ((char*)table_cur);
   uprintf("%-25s 0x%x = %d >= %d * %d",
           "table_end - table_cur",
           _UV(sizeof_table), _UV(sizeof_table),
           _UV(sizeof_table)/sizeof(*table_cur), sizeof(*table_cur));
   uprintf("</ts_print_status>");
+#else
+  /** \bug Why do we need to calculate this at run time??? */
+  uprintf("table size: %d elements", _UV(sizeof_table)/sizeof(*table_cur));
+#endif
 }
 
 

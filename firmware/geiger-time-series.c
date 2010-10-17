3d4528aae6bedf4cea0326711e7054bc60aae43a
Hans Ulrich Niedermann <hun@n-dimensional.de>
2010-10-17 12:08:09 +0200

    firmware: The __do_clear_bss stuff is (C) FSF

--- firmware/geiger-time-series.c
+++ firmware/geiger-time-series.c
@@ -3,6 +3,8 @@
  *
  * \author Copyright (C) 2010 samplemaker
  * \author Copyright (C) 2010 Hans Ulrich Niedermann <hun@n-dimensional.de>
+ * \author Copyright (C) 1998, 1999, 2000, 2007, 2008, 2009 Free Software Foundation, Inc.
+ *         (for the assembly code in ts_init() to clear data_table)
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public License

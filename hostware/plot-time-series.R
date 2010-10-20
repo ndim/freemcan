f7065d6a89019dd6834f571bb957f003aa3a30ba
Hans Ulrich Niedermann <hun@n-dimensional.de>
2010-10-20 15:35:04 +0200

    Add plot-time-series.R script

---
+++ hostware/plot-time-series.R
@@ -0,0 +1,100 @@
+#!/usr/bin/Rscript
+# plot-time-series.R - plot a time series dat file using R
+# Copyright (C) 2010 samplemaker
+# Copyright (C) 2010 Hans Ulrich Niedermann
+#
+# Usage: ./plot-time-series.R <period> <filename>
+#
+# Example usage:
+#
+#  $ ./plot-time-series.R 150 time.foobar.dat
+#
+#    Plot a data file of "time.foobar.dat" recorded with 150 seconds
+#    per value.
+#
+#  $ ./plot-time-series.R 30 $(ls -1 time.*.dat | tail -n1)
+#
+#    Plot the latest file, presuming it is recorded at 30sec/value.
+#
+
+
+# clear workspace
+rm(list = ls())
+
+
+# this is a poor mans infinite impulse response filter of first order (PT1).
+pt1  <- function(x,k) {
+  Ta <- 1
+  T <- k*Ta
+  n <- 2*T/Ta;
+  a <- 1/(n+1);
+  b <- (n-1)/(n+1);
+
+  z <- x[1]/(2.0*a);
+  for (i in 1:length(x)) {
+    m <- x[i];
+    tmp <- b*z;
+    x[i] <- a*(m+tmp+z);
+    z <- tmp+m;
+  }
+  return(x);
+}
+
+# calculate theoretical one sigma quantil of the distribution
+quantil <- function(x) {
+  len <- length(x)
+  cat("samples parsed:", len, "\n")
+  mean <- mean(x)
+  cat("mean value:", mean, "\n")
+  sigma  <- sqrt(mean)
+  cat("your one sigma probability is within: +/- ", sigma, " counts \n")
+  return(c(mean-sigma, mean+sigma, mean));
+}
+
+# \fixme: one should filter the correct command rather than give a fix position
+args <- commandArgs(TRUE)
+period <- as.numeric(args[1])
+filename <- args[2]
+cat("Period:", period, "\n")
+cat("Filename:", filename, "\n")
+
+pfact <- 60.0 / period
+
+# read log file as dataframe
+histdata<-read.table(filename, header=FALSE, sep="\t")
+# move the columns from dataframe into a single vector respectively and subtract the last (inclompete) measurement
+len <-  length(histdata[,1]) - 1
+channel <- histdata[,1][0:len]
+counts <- histdata[,2][0:len]
+pfcounts = pfact * counts
+q <- quantil(counts)
+cat("Quantil:", "min", q[1], "max", q[2], "mean", q[3], "\n")
+pfq <- pfact * q
+cat("Quantil:", "min", pfq[1], "max", pfq[2], "mean", pfq[3], "\n")
+
+x11(width=10,height=8)
+# 2 rows, 1 columns.
+par(mfrow=c(2,1))
+
+# plot low pass filtered counts over measurement
+plot(channel,pt1(pfcounts,5) , main = paste("Raw data from:",filename), type = "l",col = "red",xlab = "Number of measurement",ylab=paste("cpm (scaled from", period, "seconds)"), ylim=c(min(pfcounts),max(pfcounts)))
+# plot raw counts over measurement into the same first plot
+lines(channel, pfcounts , type="l", col="darkgreen")
+# plot mean +- sigma lines
+abline(h=pfq[1], col="blue")
+abline(h=pfq[2], col="blue")
+abline(h=pfq[3], col="blue")
+
+# from the data create a histogram
+hx <- hist(pfcounts, breaks = 90, plot = FALSE)
+# plot the histogram into the second plot. plot blue if the bar is below one sigma otherwise red
+plot(hx, col = ifelse(((hx$breaks < pfq[1]) | (hx$breaks > pfq[2])), "blue", "red"), xlab=paste("cpm (counts per minute, scaled from original measurement period of", period, "seconds)"))
+# plot mean +- sigma lines
+abline(v=pfq[1], col="darkgreen", lwd=2)
+abline(v=pfq[2], col="darkgreen", lwd=2)
+abline(v=pfq[3], col="darkgreen", lwd=2)
+
+# The `locator' function waits for you to either click the mouse or hit enter (putting coordinates into x & y)
+z <- locator(1)
+q()
+

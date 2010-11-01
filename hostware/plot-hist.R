c3400cda267540ad883a6cbc7386cb93fc42b928
samplemaker <@>
2010-11-02 00:06:34 +0100

    support of background

--- hostware/plot-hist.R
+++ hostware/plot-hist.R
@@ -1,5 +1,5 @@
 #!/usr/bin/Rscript
-# plot-time-series.R - plot a time series dat file using R
+# plot-hist.R - plot a multi channel histogram data file using R
 # Copyright (C) 2010 samplemaker
 # Copyright (C) 2010 Hans Ulrich Niedermann
 #
@@ -7,10 +7,10 @@
 #
 # Example usage:
 #
-# ./plot-hist.R filename=hist.2010-10-31.19\:21\:42.D.dat tubesensitivity=0.060584 duration=1200
+# ./plot-hist.R filename=hist.2010-11-01.17\:55\:01.R.dat bg=hist.2010-11-01.17\:34\:17.D.dat tubesensitivity= duration=1200
 #
-# # Ludlum  44-2: tubesensitivity=0.060584
-#
+# # Ludlum  44-2:  tubesensitivity = 0.060584
+# # Ludlum  44-11: tubesensitivity =
 #
 
 # clear workspace
@@ -62,7 +62,7 @@ args <- commandArgs(TRUE)
 for (numarg in 1:length(args)) {
   cmd = strsplit(args[numarg],split="=")
   if(! is.na(cmd[[1]][2])) {
-    if (cmd[[1]][1] == "filename"){
+    if ((cmd[[1]][1] == "filename") | (cmd[[1]][1] == "bg")){
       assign(cmd[[1]][1],cmd[[1]][2])
     }
     else{
@@ -81,37 +81,53 @@ for (numarg in 1:length(args)) {
 # vector respectively and subtract the last (inclompete) measurement
 histdata<-read.table(filename, header=TRUE, sep="\t")
 len <-  length(histdata[,1]) - 1
-counts <- histdata$count
-index <- histdata$channel
+#skip last channel (residual counts)
+counts <- histdata$count[seq(1,len,1)]
+index <- histdata$channel[seq(1,len,1)]
 
-# one sigma of the overall distribution and accuracy of the overal mean value:
-overallmean <- 60*sum(counts)/duration
-overallmeanaccuracy <- sqrt(overallmean)
-
+if(bg == FALSE){
+  #if there is no background data available set bg to zero
+  len_bg <-  len
+  counts_bg <- rep(0, len)
+  index_bg <- histdata$channel[seq(1,len,1)]
+  # one sigma of the overall distribution and accuracy of the overal mean value:
+  overallmean <- 60*sum(histdata$count)/duration
+  overallmeanaccuracy <- sqrt(overallmean)
+  residual_counts <- histdata$count[len+1]
+}else{
+  histdata_bg<-read.table(bg, header=TRUE, sep="\t")
+  len_bg <-  length(histdata_bg[,1]) - 1
+  #skip last channel
+  counts_bg <- histdata_bg$count[seq(1,len_bg,1)]
+  index_bg <- histdata_bg$channel[seq(1,len_bg,1)]
+  # one sigma of the overall distribution and accuracy of the overal mean value:
+  overallmean <- 60*sum(histdata$count-histdata_bg$count)/duration
+  overallmeanaccuracy <- sqrt(overallmean)
+  residual_counts <- histdata$count[len+1] - histdata_bg$count[len+1]
+}
 
 # configure the plot output and plotting area (2 rows, 1 columns)
-x11(width=10,height=4.5)
-#postscript(width=10, height=8, horizontal=TRUE)
-par(mfrow=c(1,1))
+x11(width=8,height=8)
+# postscript(width=10, height=8, horizontal=TRUE)
+par(mfrow=c(2,1))
 
-# in the first plot the filtered rawdata and the unfiltered rawdata is plot including 
-# one sigma thresholds 
+### in the first chart the gamma spectra minus background is plot
 #par(mar=c(5, 5, 5, 5))  #plot area margins
 
-plot(index,counts, type="l", col="red", main = paste("Datastream from:",filename), 
+plot(index,counts-counts_bg, type="l", col="red", main = paste("Gamma spectra from logged data"), 
      xlab=" ", ylab=" ", ylim=c(min(0),max(counts)))
 
-# two calibration points
-genergies <- cbind(c(87,202),c(130,307))
-abline(v=genergies[1,], lty="dashed",lwd=1, col="blue")
-text(genergies[1,], counts[genergies[1,]], genergies[2,], col="black", lwd=1, pos=4)
-#text(genergies[1,], max(counts), genergies[2,], col="black", lwd=1, pos=4)
+# two calibration points for energy axis
+genergies <- cbind(c(88,239),c(213,583))
+abline(v=genergies[1,], lty="dashed",lwd=1, col="darkgrey")
+text(genergies[1,], counts[genergies[1,]], paste(genergies[2,],"keV"), col="black", lwd=1, pos=4, cex=0.8)
 
 # from the two calibration points calculate the transfer function between channel & energy
+# genergy <- b+m*channel
 m <- (genergies[2,1]-genergies[2,2])/(genergies[1,1]-genergies[1,2])
 b <- genergies[2,2]-genergies[1,2]*m
 
-# determine a nice looking labeling of the energy axis
+# calculate a nice looking energy axis labeling
 axisenergies = c(b,b+m*max(index))
 axiscounts = c(0,length(index))
 newaxis <- axisrescaler(axiscounts,axisenergies)
@@ -121,23 +137,13 @@ axis(1,line=2.5,col="darkgrey",at=tickposition, labels = ticklabel)
 
 mtext("total counts", adj=0,side=2, line=2.5,cex=1)
 
-#plot(index, counts ,lwd=0.5, col="darkgreen", type="l", ann=FALSE, yaxt="n", 
-#     ylim=c(min(0),max(counts)))
-
-
-par(new=TRUE)
-
-#countsfiltered <- counts
-#plot(index,countsfiltered, type="l", col="red", main = paste("Datastream from:",filename), 
-#     xlab=" ", ylab=" ", ylim=c(min(0),max(counts)))
-
-
 if(tubesensitivity == FALSE){
   legend(x="topright", bty="n",cex=0.8, legend=c(  
          paste("overall average:",round(overallmean,2),"CPM"),
          "overall one sigma-",
          paste("accuracy:+/-",round(overallmeanaccuracy,2),"CPM"),
-         paste("total time:",duration,"sec") ))
+         paste("total time:",duration,"sec"),
+         paste("residual counts:", residual_counts) ))
 }else{
   legend(x="topright", bty="n",cex=0.8, legend=c(  
          paste("overall average:",round(overallmean*tubesensitivity,2),"nSv/h"),
@@ -146,6 +152,24 @@ if(tubesensitivity == FALSE){
          paste("total time:",duration,"sec") ))
 }
 
+
+### second plot: unprocessed raw data
+plot(index_bg,counts_bg, type="l", col="darkgreen", main = paste("Data:",bg,filename), 
+     xlab=" ", ylab=" ", ylim=c(min(0),max(counts)))
+par(new=TRUE)
+plot(index, counts ,lwd=0.5, col="darkblue", type="l", ann=FALSE, yaxt="n", 
+     ylim=c(min(0),max(counts)))
+axis(1,line=2.5,col="darkgrey",at=tickposition, labels = ticklabel)
+
+#some prominent gamma energies for Th-232 decay chain 
+genergies <- cbind(c(239,"Pb-212"),c(727,"Bi-212"),c(911,"Ac-228"),c(969,"Ac-228"),c(338,"Ac-228"),c(583,"Tl-208"))
+# channelNo <- (genergy-b)/m
+channels <- (as.integer(genergies[1,])-b)/m
+abline(v=channels, lty="dashed",lwd=1, col="darkgrey")
+text(channels, 0.6*max(counts), paste(genergies[2,],"(",genergies[1,],"keV)"), col="black", 
+     lwd=1, pos=4,cex = 0.6, srt = 90)
+
+mtext("total counts", adj=0,side=2, line=2.5,cex=1)
 
 # The `locator' function waits for you to either click the mouse
 z <- locator(1)

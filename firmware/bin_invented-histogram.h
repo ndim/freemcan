54a4368c7a29e5e21ad188846d6d5a1a621de18d
Hans Ulrich Niedermann <hun@n-dimensional.de>
2015-09-14 03:24:51 +0200

    fw: New perso sim-mca: uses invented histogram
    The new sim-mca firmware personality using invented histogram
    data is only built if Erlang is installed, because generating
    the invented histogram is done by an Erlang program at firmware
    build time.
    
    Note: This code is untested.

---
+++ firmware/bin_invented-histogram.h
@@ -0,0 +1,60 @@
+/** \file firmware/bin_invented-histogram.h
+ * \brief Global adjustments for freemcan firmware
+ *
+ * \author Copyright (C) 2010 samplemaker
+ * \author Copyright (C) 2010 Hans Ulrich Niedermann <hun@n-dimensional.de>
+ * \author Copyright (C) 2015 Hans Ulrich Niedermann <hun@n-dimensional.de>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1
+ *  of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free
+ *  Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *  Boston, MA 02110-1301 USA
+ *
+ *
+ * \todo Add bin_invented-histogram.S to doxygen somehow (doxygen cannot read assembly language by default).
+ *
+ * \addtogroup perso_sim_mac
+ * @{
+ */
+
+#ifndef BIN_INVENTED_HISTOGRAM_H
+#define BIN_INVENTED_HISTOGRAM_H
+
+#include <stdlib.h>
+
+
+/** Simulated symbol for the size of the #data_table object.
+ *
+ * This simulated symbol contains the size of #data_table object as
+ * the value of its absolute address. The linker can fill in this
+ * value in code, so you can use this as an initialization value in C
+ * code - given the proper type casting (see #DATA_TABLE_SIZE).
+ */
+extern char data_table_size[];
+
+/** The correct type casting for #data_table_size.
+ */
+#define DATA_TABLE_SIZE ((size_t)((void *)data_table_size))
+
+#endif /* BIN_INVENTED_HISTOGRAM_H */
+
+
+/** @} */
+
+
+/*
+ * Local Variables:
+ * c-basic-offset: 2
+ * indent-tabs-mode: nil
+ * End:
+ */
---
+++ firmware/perso-sim-mca.c
@@ -0,0 +1,80 @@
+/** \file firmware/perso-sim-mca.c
+ * \brief Personality: MCA simulation with invented histogram
+ *
+ * \author Copyright (C) 2010 samplemaker
+ * \author Copyright (C) 2010 Hans Ulrich Niedermann <hun@n-dimensional.de>
+ * \author Copyright (C) 2015 Hans Ulrich Niedermann <hun@n-dimensional.de>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1
+ *  of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free
+ *  Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *  Boston, MA 02110-1301 USA
+ *
+ * \defgroup perso_sim_mac Personality: MCA simulation with invented histogram
+ * \ingroup firmware_personality_groups
+ *
+ * @{
+ */
+
+
+#define BYTES_PER_VALUE 3
+#define BITS_PER_VALUE (BYTES_PER_VALUE * 8)
+
+#include "global.h"
+#include "main.h"
+#include "data-table.h"
+
+#include "bin_invented-histogram.h"
+
+
+/** See * \see data_table */
+data_table_info_t data_table_info = {
+  /** Actual size of #data_table in bytes */
+  DATA_TABLE_SIZE,
+  /** Type of value table we send */
+  VALUE_TABLE_TYPE_HISTOGRAM,
+  /** Table element size */
+  BITS_PER_VALUE,
+};
+
+/** See * \see data_table */
+PERSONALITY("sim-mca",
+            2,0,
+            1,
+            DATA_TABLE_SIZE,
+            BITS_PER_VALUE);
+
+
+/** \todo Should give out a reasonable value */
+uint16_t get_duration(void)
+{
+  return 0;
+}
+
+void personality_start_measurement_sram(void)
+{
+}
+
+void on_measurement_finished(void)
+{
+}
+
+
+/** @} */
+
+/*
+ * Local Variables:
+ * c-basic-offset: 2
+ * indent-tabs-mode: nil
+ * End:
+ */

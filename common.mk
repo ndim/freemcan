fc9226fe10e77a70d8291fb3ffac371fa56fb62c
Hans Ulrich Niedermann <hun@n-dimensional.de>
2016-01-20 02:16:43 +0100

    common.mk: make it work in different dir levels

--- common.mk
+++ common.mk
@@ -1,6 +1,7 @@
 # Common make parts
 
-FOO_1 := $(shell ../git-version.sh git-version.h)
+TOP_DIR := $(shell for d in . .. ../..; do if test -f "$$d/git-version.sh"; then echo "$$d"; exit; fi; done; exit 1)
+FOO_1 := $(shell $(TOP_DIR)/git-version.sh git-version.h)
 CLEANFILES += git-version.h
 
 HAVE_ERL := $(shell for f in {,/usr}/lib{,64}/erlang/bin/erl; do if test -e "$$f"; then echo yes; exit; fi; done; echo no)
